3fe17c5c7bb29ef478cb888cea6a4bedfba024e5
Alessandro Paganelli <alessandro.paganelli@zuru.com>
2025-07-01 16:01:11 +0200

    Green test for File struct

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,47 @@
+use regex::Regex;
+
 mod project {
+
     pub struct File {
         name: String,
-        used_symbols: Vec<String>,
-        defined_symbols: Vec<String>,
+        used_modules: Vec<String>,
     }
 
-    pub struct Directory {
-        name: String,
-        files: Vec<File>,
-        directories: Vec<Box<Directory>>,
+    impl File {
+        pub fn make(name: &str, file_content: &str) -> Result<File, &'static str> {
+            let used_modules = File::make_used_modules(file_content)?;
+
+            Ok(File {
+                name: String::from(name),
+                used_modules: used_modules,
+            })
+        }
+
+        pub fn get_name(&self) -> &str {
+            &self.name
+        }
+
+        pub fn get_used_modules(&self) -> &[String] {
+            &self.used_modules
+        }
+
+        fn make_used_modules(file_content: &str) -> Result<Vec<String>, &'static str> {
+            let re = super::Regex::new(r#"^\s*#include\s*[<"](.*?)[>"](?:\s*//.*)?$"#)
+                .map_err(|_| "Error in regex creation")?;
+
+            let used_modules = file_content
+                .lines()
+                .filter(|row| !row.trim_start().starts_with("//"))
+                .filter(|row| !row.trim_start().starts_with("/*"))
+                .filter_map(|row| {
+                    re.captures(row)
+                        .and_then(|captures| captures.get(1))
+                        .map(|m| m.as_str().to_string())
+                })
+                .collect();
+
+            Ok(used_modules)
+        }
     }
 }
 
@@ -17,7 +50,28 @@ mod tests {
     use super::*;
 
     #[test]
-    fn basic_moves_creation_test() {
-        assert_eq!(14, 15);
+    fn cpp_file_parsing_test() -> Result<(), &'static str> {
+        let file_name = "main.cpp";
+        let f = project::File::make(
+            file_name,
+            "\
+#include <iostream>
+#include \"foobar.h\"
+//#include \"commented_out.h\"
+/*#include \"another_commented_out.h\"
+
+int main(void) {
+    printf(\"Hello world\");
+    return 0;
+};",
+        )?;
+
+        assert_eq!(file_name, f.get_name());
+        assert_eq!(
+            vec![String::from("iostream"), String::from("foobar.h")],
+            f.get_used_modules()
+        );
+
+        Ok(())
     }
 }
