e1717edafcf84d545f1f16d21b30f80566c115c0
Alessandro Paganelli <alessandro.paganelli@zuru.com>
2025-07-03 08:28:12 +0200

    Green tests for path parsing

--- src/modules_analyzer.rs
+++ src/modules_analyzer.rs
@@ -10,6 +10,13 @@ impl<'a> ModulesAnalyzer<'a> {
     pub fn make(files: &'a [File]) -> Result<ModulesAnalyzer, Box<dyn Error>> {
         todo!()
     }
+
+    pub fn extract_filename_from_path(path: &str) -> &str {
+        match path.split("/").last() {
+            Some(last_token) => last_token,
+            None => path,
+        }
+    }
 }
 
 #[cfg(test)]
@@ -63,6 +70,7 @@ void DoSomeStuff(uint8_t value) {}
         Ok(vec![first, second, third])
     }
 
+    #[ignore]
     #[test]
     fn simple_parsing_test() -> Result<(), Box<dyn Error>> {
         let files = create_sample_files()?;
@@ -70,5 +78,35 @@ void DoSomeStuff(uint8_t value) {}
         let analyzer = ModulesAnalyzer::make(&files)?;
 
         Ok(())
+    }
+
+    #[test]
+    fn path_parsing_test_simple() {
+        let simple_path = "include/foobar.h";
+        assert_eq!(
+            "foobar.h",
+            ModulesAnalyzer::extract_filename_from_path(simple_path)
+        );
+    }
+
+    #[test]
+    fn path_parsing_test_single_token() {
+        let simple_path = "foobar.h";
+        assert_eq!(
+            "foobar.h",
+            ModulesAnalyzer::extract_filename_from_path(simple_path)
+        );
+    }
+
+    #[test]
+    fn path_parsing_test_backslash() {
+        // Note: C and C++ standards explicitly say that it's UB using something other than '/'.
+        // Hence here we assume that our logic has to report the full string slice in case of
+        // backslash.
+        let simple_path = "include\\foobar.h";
+        assert_eq!(
+            "include\\foobar.h",
+            ModulesAnalyzer::extract_filename_from_path(simple_path)
+        );
     }
 }
