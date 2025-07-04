7d8fb146f201d41e6d6d4e7995149c7ad2358304
Alessandro Paganelli <alessandro.paganelli@zuru.com>
2025-07-04 11:59:21 +0200

    Moves topn use-case inside the corresponding module

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,5 @@
-use dependency_analyzer::DependencyAnalyzer;
 use std::error::Error;
-use std::path::Path;
+use use_cases::TopNUseCase;
 
 pub mod dependency_analyzer;
 pub mod file;
@@ -10,6 +9,7 @@ pub mod use_cases;
 #[derive(Debug)]
 pub struct Config {
     project_path: String,
+    is_topn_analyzer: bool,
 }
 
 impl Config {
@@ -18,34 +18,25 @@ impl Config {
             return Err(String::from("Not enough arguments").into());
         }
 
-        Ok(Config {
-            project_path: args[1].clone(),
-        })
+        // check mode
+        if args.len() > 2 {
+            Ok(Config {
+                project_path: args[1].clone(),
+                is_topn_analyzer: args[2] == "-t",
+            })
+        } else {
+            Ok(Config {
+                project_path: args[1].clone(),
+                is_topn_analyzer: false,
+            })
+        }
     }
 }
 
 pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
-    let path = Path::new(&config.project_path);
-    let mut project = project_scanner::ProjectScanner::make(path)?;
-
-    // FIXME: this should go in a use-case
-    let files = project.scan_files()?;
-    let analyzer = DependencyAnalyzer::make(&files)?;
-
-    println!("Sorting ...");
-    let sorted_inclusions = analyzer.get_sorted_inclusion();
-
-    println!("Sorted!");
-    for i in sorted_inclusions[..50].iter() {
-        println!(
-            "Include found: {}, num inclusions: {}",
-            i.get_file_name(),
-            i.get_including_file_paths().len()
-        );
+    if config.is_topn_analyzer {
+        TopNUseCase::do_sorted_topn_inclusions(&config.project_path, 10)?;
     }
 
     Ok(())
 }
-
-#[cfg(test)]
-mod tests {}
---
+++ src/use_cases.rs
@@ -0,0 +1,30 @@
+use crate::dependency_analyzer::DependencyAnalyzer;
+use crate::project_scanner::ProjectScanner;
+use std::error::Error;
+use std::path::Path;
+
+pub struct TopNUseCase {}
+
+impl TopNUseCase {
+    pub fn do_sorted_topn_inclusions(path: &str, num: usize) -> Result<(), Box<dyn Error>> {
+        let path = Path::new(path);
+        let mut project = ProjectScanner::make(path)?;
+
+        let files = project.scan_files()?;
+        let analyzer = DependencyAnalyzer::make(&files)?;
+
+        println!("Sorting ...");
+        let sorted_inclusions = analyzer.get_sorted_inclusion();
+
+        println!("Sorted!");
+        for i in sorted_inclusions[..num].iter() {
+            println!(
+                "Include found: {}, num inclusions: {}",
+                i.get_file_name(),
+                i.get_including_file_paths().len()
+            );
+        }
+
+        Ok(())
+    }
+}
