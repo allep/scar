c0c966062ad8479999191d06468850893b1733ba
Alessandro Paganelli <alessandro.paganelli@zuru.com>
2025-07-08 08:46:25 +0200

    Moves tests, adds use-case tests

--- src/use_cases.rs
+++ src/use_cases.rs
@@ -2,6 +2,7 @@ use crate::dependency_analyzer::DependencyAnalyzer;
 use crate::dependency_analyzer::DependencyEntry;
 use crate::project_scanner::ProjectScanner;
 use std::cmp::Ordering;
+use std::collections::HashMap;
 use std::error::Error;
 use std::path::Path;
 
@@ -15,7 +16,24 @@ fn get_slice_up_to<T>(slice: &[T], num: usize) -> &[T] {
 pub struct TopNUseCase {}
 
 impl TopNUseCase {
-    pub fn do_sorted_topn_inclusions(path: &str, num: usize) -> Result<(), Box<dyn Error>> {
+    fn make_output_data_from_slice(
+        dependency_entries: &[DependencyEntry],
+    ) -> HashMap<String, usize> {
+        dependency_entries
+            .iter()
+            .map(|e| {
+                (
+                    e.get_file_name().to_string(),
+                    e.get_including_file_paths().len(),
+                )
+            })
+            .collect()
+    }
+
+    pub fn do_sorted_topn_inclusions(
+        path: &str,
+        num: usize,
+    ) -> Result<HashMap<String, usize>, Box<dyn Error>> {
         let path = Path::new(path);
         let mut project = ProjectScanner::make(path)?;
 
@@ -26,8 +44,7 @@ impl TopNUseCase {
         let sorted_inclusions = analyzer.get_sorted_inclusion();
         println!("Sorted!");
 
-        let sorted_inclusions: &[DependencyEntry] = get_slice_up_to(&sorted_inclusions, num);
-
+        let sorted_inclusions = get_slice_up_to(&sorted_inclusions, num);
         for i in sorted_inclusions.iter() {
             println!(
                 "Source found: {}, num inclusions: {}",
@@ -36,10 +53,13 @@ impl TopNUseCase {
             );
         }
 
-        Ok(())
+        Ok(Self::make_output_data_from_slice(sorted_inclusions))
     }
 
-    pub fn do_sorted_topn_impact(path: &str, num: usize) -> Result<(), Box<dyn Error>> {
+    pub fn do_sorted_topn_impact(
+        path: &str,
+        num: usize,
+    ) -> Result<HashMap<String, usize>, Box<dyn Error>> {
         let path = Path::new(path);
         let mut project = ProjectScanner::make(path)?;
 
@@ -61,7 +81,7 @@ impl TopNUseCase {
             );
         }
 
-        Ok(())
+        Ok(Self::make_output_data_from_slice(sorted_impacts))
     }
 }
 
@@ -77,5 +97,27 @@ mod tests {
         assert_eq!(vec![1, 2, 3, 4, 5, 6], get_slice_up_to(&elements, 6));
         assert_eq!(vec![1, 2, 3, 4, 5, 6], get_slice_up_to(&elements, 7));
         assert_eq!(vec![1, 2, 3, 4, 5, 6], get_slice_up_to(&elements, 1000));
+    }
+
+    #[test]
+    fn integration_use_case_inclusion_simple() -> Result<(), Box<dyn Error>> {
+        let inclusions = TopNUseCase::do_sorted_topn_inclusions("tests/simple", 100)?;
+        assert_eq!(4, inclusions.len());
+        assert_eq!(2, inclusions["test001.h"]);
+        assert_eq!(1, inclusions["test002.h"]);
+        assert_eq!(0, inclusions["test001.cpp"]);
+        assert_eq!(0, inclusions["test002.cpp"]);
+        Ok(())
+    }
+
+    #[test]
+    fn integration_use_case_impact_simple() -> Result<(), Box<dyn Error>> {
+        let impacts = TopNUseCase::do_sorted_topn_impact("tests/simple", 100)?;
+        assert_eq!(4, impacts.len());
+        assert_eq!(3, impacts["test001.h"]);
+        assert_eq!(1, impacts["test002.h"]);
+        assert_eq!(0, impacts["test001.cpp"]);
+        assert_eq!(0, impacts["test002.cpp"]);
+        Ok(())
     }
 }
